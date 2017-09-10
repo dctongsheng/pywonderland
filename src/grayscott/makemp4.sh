982910b98abcd62a46b844efca651518ae4d9cfa
neozhaoliang <mathzhaoliang@gmail.com>
2017-09-10 20:05:12 +0800

    frame rate = 24

--- src/grayscott/makemp4.sh
+++ src/grayscott/makemp4.sh
@@ -2,4 +2,4 @@
 
 rm grayscott.mp4
 
-ffmpeg -framerate 30 -i frames/frame%05d.png -c:v libx264 -crf 20 -b:v 2M grayscott.mp4
+ffmpeg -framerate 24 -i frames/frame%05d.png -c:v libx264 -crf 20 -b:v 2M grayscott.mp4
